3ea5ae89a8b451ee471b85cb8ec49b6b8dc3e93a
JW230321 <128436266+JW230321@users.noreply.github.com>
2023-07-20 17:43:13 +0900

    안준원 - Member_ver1
    프론트에 있는 리엑트 회원가입 로그인 ver1 과 함께 다운로드 권장

---
+++ src/main/java/com/siat/web/Member/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.siat.web.Member;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>{
+	Member findById(String Id);
+}
