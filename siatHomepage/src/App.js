ee94e1d30a3aa5c38cb4c7654db4d401d593a6b5
ilsoonp <128436282+ilsoonp@users.noreply.github.com>
2023-07-25 18:06:27 +0900

    일순.notice리액트
    아직 페이징,검색기능은 없음

--- siatHomepage/src/App.js
+++ siatHomepage/src/App.js
@@ -1,14 +1,18 @@
+
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
 import Board1 from "./components/Board1";
-import ProcessComponent from "./register_components/ProcessComponent";
-import RequestComponent from "./register_components/RequestComponent";
-import ProcessDetail from "./register_components/ProcessDetail";
-import ProcessDetail2 from "./register_components/ProcessDetail2"
-import ProcessInput from "./register_components/ProcessInput";
-import ProcessInput2 from "./register_components/ProcessInput2"
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Notice from "./noticeComponents/Notice";
+import InsertNotice from "./noticeComponents/InsertNotice";
+import DetailNotice from "./noticeComponents/DetailNotice";
+import ModifyNotice from "./noticeComponents/ModifyNotice";
+
+
+
 
 function App() {
   return (
@@ -17,12 +21,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/board1" element={<Board1/>} />
-        <Route path="/process" element={<ProcessComponent />} />
-        <Route path="/request" element={<RequestComponent />} />
-        <Route path="/detail" element={<ProcessDetail />} />
-        <Route path="/detail2" element={<ProcessDetail2 />} />
-        <Route path="/input" element={<ProcessInput />} />
-        <Route path="/input2" element={<ProcessInput2 />} />
+        <Route path="/admin" element={<Board1/>} />
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/noticeList" element={<Notice/>} />
+        <Route path="/insertNotice" element={<InsertNotice />} />
+        <Route path="/detailNotice/:notice_id" element={<DetailNotice  />} />
+        <Route path="/modifyNotice/:notice_id" element={<ModifyNotice />}/>
       </Routes>
       <Footer />
     </div>
