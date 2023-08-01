3288807cc86b26d05bdc7f48f403a72688a5a39d
ilsoonp <128436282+ilsoonp@users.noreply.github.com>
2023-08-01 13:47:02 +0900

    일순 홈페이지 자유게시판 수정페이지 UI업데이트

--- react-siatproj/src/components/Home.js
+++ react-siatproj/src/components/Home.js
@@ -1,156 +1,34 @@
-import HomeBanner from "./HomeBanner";
-import '../css/home.css';
 function Home() {
-    
     return (
-        <>
+        <>  
             {/* 이미지 화면 */}
-            {/* <section id="hero" className="d-flex flex-column justify-content-center align-items-center"></section> */}
-            <HomeBanner />
-
+            <section id="hero" class="d-flex flex-column justify-content-center align-items-center"></section>
             {/* 게시판 목록 */}
-            <div className="container text-center" style={{ marginTop: 40 }}>
-                <div className="row">
-                    <div className="left_box">
-                        <div className="tit">
-                            <h4>공지 사항</h4>
-                            <a href="#"><img src="https://www.sist.co.kr/img/main_con02_plus.png" alt="icon" /></a>
-                        </div>
-                        <li className="slick-slide">
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                        </li>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <ul>
+                            <h1>공지사항</h1>
+                            <li>1. 제목입니다</li>
+                            <li>2. 제목입니다</li>
+                            <li>3. 제목입니다</li>
+                        </ul>
                     </div>
-                    <div className="center_box">
-                        <div className="tit">
-                            <h4>공지 사항</h4>
-                            <a href="#"><img src="https://www.sist.co.kr/img/main_con02_plus.png" alt="icon" /></a>
-                        </div>
-                        <li className="slick-slide">
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                        </li>
+                    <div class="col">
+                        <ul>
+                            <h1>공지사항</h1>
+                            <li>1. 제목입니다</li>
+                            <li>2. 제목입니다</li>
+                            <li>3. 제목입니다</li>
+                        </ul>
                     </div>
-                    <div className="right_box">
-                        <div className="tit">
-                            <h4>공지 사항</h4>
-                            <a href="#"><img src="https://www.sist.co.kr/img/main_con02_plus.png" alt="icon" /></a>
-                        </div>
-                        <li className="slick-slide">
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                            <ul>
-                                <dl style={{ display: "flex", alignItems: "center" }}>
-                                    <dt>
-                                        <h1>1.</h1>
-                                    </dt>
-                                    <dt style={{ fontSize: 18, width: 150 }}>
-                                        제목입니다.
-                                    </dt>
-                                    <dt>
-                                        <a href="#"><img src="https://www.sist.co.kr/img/main_con02_arrow02.png" /></a>
-                                    </dt>
-                                </dl>
-                            </ul>
-                        </li>
+                    <div class="col">
+                        <ul>
+                            <h1>공지사항</h1>
+                            <li>1. 제목입니다</li>
+                            <li>2. 제목입니다</li>
+                            <li>3. 제목입니다</li>
+                        </ul>
                     </div>
                 </div>
             </div>
