190ab8bbd20ca128f3c3c7c65290c4075b1b0375
JW230321 <128436266+JW230321@users.noreply.github.com>
2023-08-09 19:18:16 +0900

    member 댓글 및 게시판 외래키 설정

--- Siat-Homepage/src/main/java/com/siat/web/member/Member.java
+++ Siat-Homepage/src/main/java/com/siat/web/member/Member.java
@@ -1,9 +1,19 @@
 package com.siat.web.member;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.siat.web.board.Board;
+import com.siat.web.comment.Comment;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,4 +35,11 @@ public class Member {
 	private String password;
 	private String selectOption;
 	
+	@JsonIgnore
+	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Comment> comments;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Board> board;
 }
